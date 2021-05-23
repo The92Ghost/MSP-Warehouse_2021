0089f8e434fce0de4135e957222842d93a8d71a4
The92Ghost <rayne1992@gmail.com>
2021-05-23 15:33:58 +0300

    The92Ghost <rayne1992@gmail.com>
    Form 7 completed, added the code for the buttons.

--- Form7.cs
+++ Form7.cs
@@ -16,5 +16,42 @@ namespace MSP_Warehouse_2021
         {
             InitializeComponent();
         }
+
+        private void registrationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.Show();
+        }
+
+        private void itemsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form3 frm = new Form3();
+            frm.Show();
+        }
+
+        private void warehousesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form4 frm = new Form4();
+            frm.Show();
+        }
+
+        private void warehouseTypesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form5 frm = new Form5();
+            frm.Show();
+        }
+
+        private void inventoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form6 frm = new Form6();
+            frm.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Довиждане! :)", "Изход");
+            Application.Exit();
+        }
+
     }
 }
