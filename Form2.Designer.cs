cdde37f4e2f807806a673e2295f6fad1ae5b44ce
Radoslav <58222381+RadoslavKK@users.noreply.github.com>
2021-05-26 00:43:53 +0300

    <radoslavkostovpc@gmail.com> RadoslavKK

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -112,7 +112,7 @@ namespace MSP_Warehouse_2021
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(429, 356);
+            this.button1.Location = new System.Drawing.Point(443, 356);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(113, 46);
