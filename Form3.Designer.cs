50fba221a2baee487e81edb91b0c9c6971eff0b5
nellydiyanova <Emily_5@abv.bg>
2021-05-27 18:35:57 +0300

    nellydiyanova <Emily_5@abv.bg>
    Направени промени по дизайна на форма 3 и 4.

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -49,10 +49,11 @@ namespace MSP_Warehouse_2021
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(292, 483);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Olive;
+            this.button4.Location = new System.Drawing.Point(430, 12);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(136, 44);
+            this.button4.Size = new System.Drawing.Size(102, 36);
             this.button4.TabIndex = 80;
             this.button4.Text = "Menu";
             this.button4.UseVisualStyleBackColor = true;
@@ -60,10 +61,11 @@ namespace MSP_Warehouse_2021
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(33, 483);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Firebrick;
+            this.button3.Location = new System.Drawing.Point(293, 191);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(136, 44);
+            this.button3.Size = new System.Drawing.Size(102, 36);
             this.button3.TabIndex = 79;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = true;
@@ -71,10 +73,11 @@ namespace MSP_Warehouse_2021
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(563, 483);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button2.Location = new System.Drawing.Point(77, 191);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(136, 44);
+            this.button2.Size = new System.Drawing.Size(102, 36);
             this.button2.TabIndex = 78;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = true;
@@ -82,10 +85,11 @@ namespace MSP_Warehouse_2021
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(292, 195);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(185, 191);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(136, 44);
+            this.button1.Size = new System.Drawing.Size(102, 36);
             this.button1.TabIndex = 77;
             this.button1.Text = "Insert";
             this.button1.UseVisualStyleBackColor = true;
@@ -93,93 +97,94 @@ namespace MSP_Warehouse_2021
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(152, 148);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(138, 132);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(328, 22);
+            this.textBox3.Size = new System.Drawing.Size(247, 25);
             this.textBox3.TabIndex = 76;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(34, 154);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 139);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(109, 17);
+            this.label4.Size = new System.Drawing.Size(104, 18);
             this.label4.TabIndex = 75;
             this.label4.Text = "Item Description";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(152, 104);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(138, 96);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(328, 22);
+            this.textBox2.Size = new System.Drawing.Size(247, 25);
             this.textBox2.TabIndex = 74;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(152, 61);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(138, 62);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(328, 22);
+            this.textBox1.Size = new System.Drawing.Size(247, 25);
             this.textBox1.TabIndex = 73;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(34, 109);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 103);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.Size = new System.Drawing.Size(72, 18);
             this.label3.TabIndex = 72;
             this.label3.Text = "Item Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(34, 66);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 68);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.Size = new System.Drawing.Size(56, 18);
             this.label2.TabIndex = 71;
             this.label2.Text = "ID Item";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 17);
+            this.label1.Size = new System.Drawing.Size(69, 33);
             this.label1.TabIndex = 70;
             this.label1.Text = "Items";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(33, 279);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(24, 266);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(666, 178);
+            this.dataGridView1.Size = new System.Drawing.Size(500, 145);
             this.dataGridView1.TabIndex = 81;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(549, 48);
+            this.pictureBox1.Location = new System.Drawing.Point(420, 68);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(150, 149);
+            this.pictureBox1.Size = new System.Drawing.Size(112, 121);
             this.pictureBox1.TabIndex = 82;
             this.pictureBox1.TabStop = false;
             // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(725, 554);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(544, 450);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button4);
@@ -193,9 +198,8 @@ namespace MSP_Warehouse_2021
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form3";
-            this.Text = "Form3";
+            this.Text = "Items";
             this.Load += new System.EventHandler(this.Form3_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
